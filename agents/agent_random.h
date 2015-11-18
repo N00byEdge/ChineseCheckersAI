b028ec0b7094ddcb343d98d868d45c90e3b4c611
N00byEdge <me@hannesbredberg.se>
2015-11-18 20:11:02 +0100

    Added randomizing agent

---
+++ agents/agent_random.h
@@ -0,0 +1,19 @@
+#ifndef AGENT_RANDOM
+#define AGENT_RANDOM
+
+#include <agent.h>
+
+
+class agent_random : public agent {
+
+	public:
+
+		board_turn doTurn ( board b, int player );
+
+	protected:
+
+	private:
+
+};
+
+#endif // AGENT_RANDOM
--- agents/include_agents.h
+++ agents/include_agents.h
@@ -3,5 +3,6 @@
 
 #include <agent_human.h>
 #include <agent_scoring.h>
+#include <agent_random.h>
 
 #endif // INCLUDE_AGENTS
