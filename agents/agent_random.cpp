4a267434e83d674dbb092d1131ca07ca1e97f69a
N00byEdge <me@hannesbredberg.se>
2015-11-23 22:59:08 +0100

    Random agent now uses board functions

--- agents/agent_random.cpp
+++ agents/agent_random.cpp
@@ -6,20 +6,10 @@ board_turn agent_random::doTurn ( board b, int player ) {
     b.print ( );
     cout << endl;
 
+    auto v = b.findAllPossibleTurns ( player );
 
-    auto v = b.getPlayerTiles ( player );
+    cout << "Random agent found " << v.size ( ) << " different moves. Choosing one randomly.\n";
 
-    vector < board_turn > allPossibleTurns;
-
-    for ( int i = 0; i < v.size ( ); ++ i ) {
-        auto w = b.findAllPossibleTurns ( v [ i ], *new vector < board_move >, *new vector < pair < int, int > > );
-
-        for ( int j = 0; j < w.size ( ); ++ j )
-            allPossibleTurns.push_back ( w [ j ] );
-    }
-
-    cout << "Random agent found " << allPossibleTurns.size ( ) << " different moves. Choosing one randomly.\n";
-
-	return allPossibleTurns [ lib::randInt ( allPossibleTurns.size ( ) ) ];
+	return v [ lib::randInt ( v.size ( ) ) ];
 
 }
