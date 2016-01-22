8e0c65ec29376139ba4b9acd8e9dd795373a1509
N00byEdge <me@hannesbredberg.se>
2016-01-22 16:36:23 +0100

    Interface for training networks at launch

--- main.cpp
+++ main.cpp
@@ -25,6 +25,83 @@ int main ( ) {
         win.display();
     }
     #endif
+	
+	string networkToTrain;
+	
+	getNetworkToTrain:;
+	
+    cout << "If you want to train a network, please enter the path to the network now: ";
+    getline ( cin, networkToTrain );
+    if ( networkToTrain.size ( ) ) {
+		
+		getDatasetFile:;
+
+        cout << "Please enter the path to the dataset that you want to use: ";
+		string datasetFilePath;
+		getline ( cin, datasetFilePath );
+
+		ifstream datasetFile;
+		datasetFile.open ( datasetFilePath );
+		
+		if ( !datasetFile.is_open ( ) ) {
+			
+			cout << "Could not load network.\n";
+			goto getNetworkToTrain;
+			
+		}
+		neural_network nw ( networkToTrain );
+
+		if ( datasetFile.is_open ( ) ) {
+
+			auto datasets = lib::getDatasets ( datasetFile );
+			cout << datasets << endl;
+
+			cout << "What is the maximum accepted error? ";
+			long double maxError;
+			cin >> maxError;
+
+			cout << "What learning rate should we use? ";
+			long double phi;
+			cin >> phi;
+			
+			cout << "How often do we print progress? ";
+			long long reportTime;
+			cin >> reportTime;
+
+			cin.ignore ( );
+
+			nw.learn ( datasets, maxError, phi, reportTime );
+
+			cout << "Packprop done. Where do you want to save the file? ";
+			string outfile;
+			getline ( cin, outfile );
+
+			if ( outfile.size ( ) ) {
+
+				ofstream outFileStream;
+				outFileStream.open ( outfile );
+				if ( outFileStream.is_open ( ) ) outFileStream << nw;
+				else cout << "Could not write file.\n";
+
+			} else {
+
+				ofstream outFileStream;
+				outFileStream.open ( networkToTrain );
+				if ( outFileStream.is_open ( ) ) outFileStream << nw;
+				else cout << "Could not write file.\n";
+
+			}
+
+		} else {
+			
+			cout << "Could not open dataset file.\n";
+			goto getDatasetFile;
+			
+		}
+
+		datasetFile.close ( );
+
+    }
     int players = 2;
     cout << "How many players? ";
     cin >> players;
