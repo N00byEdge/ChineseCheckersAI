cee428f546251444a6929d978c3d628e57f0abd1
N00byEdge <me@hannesbredberg.se>
2016-01-29 18:59:32 +0100

    Fix random agent

--- agents/agent_random.cpp
+++ agents/agent_random.cpp
@@ -7,6 +7,17 @@ board_turn agent_random::doTurn ( board b, int player ) {
 	//cout << endl;
 
 	auto v = b.findAllPossibleTurns ( player );
+	
+	for ( int i = 0; i < v.size ( ); ++ i  ) {
+
+		if ( !b.canMakeTurn ( v [ i ] ) ) {
+
+			v.erase ( v.begin ( ) + i );
+			-- i;
+
+		}
+
+	}
 
 	//cout << "Random agent found " << v.size ( ) << " different moves. Choosing one randomly.\n";
 
