f0f1a5dec3595842afa3d040f75ae5459eddeac2
N00byEdge <me@hannesbredberg.se>
2016-03-08 17:01:18 +0100

    Very important ampersand

--- include/automatedGame.cpp
+++ include/automatedGame.cpp
@@ -4,7 +4,7 @@ void automatedGame::startGame (  ) {
 
 	vector < bool > hasWon ( players.size( ) + 1, false );
 	turnsToWin.resize ( players.size( ) );
-	for ( auto e: turnsToWin ) e = -1;
+	for ( auto & e: turnsToWin ) e = -1;
 
 	int turn = 0;
 
